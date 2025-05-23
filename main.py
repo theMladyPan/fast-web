6623ce963dd674df8f5154873602deb973312ce3
theMladyPan <stanislav@rubint.sk>
2025-05-23 13:55:56 +0200

    Add Jinja2 template support and update root endpoint

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
+
+# import jinja template
+from fastapi.templating import Jinja2Templates
 import time
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def root():
-    return RedirectResponse(url="/docs")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.j2",
+        {
+            "request": request,
+            "title": "FastAPI Jinja2 Template Example",
+            "heading": "FastAPI Jinja2 Template Example",
+            "content": "This is a simple example of using Jinja2 templates with FastAPI.",
+        },
+    )
 
 
 @app.get("/health")
